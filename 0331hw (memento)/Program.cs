317aafb63ce5a3f5cd469c711b65cfa3b7886206
Oleksandra <predictormagnolia@gmail.com>
2023-04-03 09:53:38 +0300

    Add project files.

---
+++ 0331hw (memento)/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _0331hw__memento_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter new text: ");
+            string Text=Console.ReadLine();
+            TextEditor NewText = new TextEditor(Text);
+            Caretaker Caretaker = new Caretaker(NewText);
+            do
+            {
+                Console.WriteLine("1.Redo my text \n2.Undo my text \n3.Exit");
+                switch (Console.ReadKey(true).KeyChar)
+                {
+                    case '1':
+                        Caretaker.Backup();
+                        Console.WriteLine("Enter new text: ");
+                        NewText.Rewrite(Console.ReadLine());
+                        break;
+                    case '2':
+                        Caretaker.Undo();
+                        break;
+                    case '3':
+                        Console.WriteLine("Exit");
+                        break;
+                    default:
+                        break;
+                }
+            }
+            while (Console.ReadKey(true).KeyChar != '3');
+        }
+    }
+}
---
+++ 0331hw (memento)/TextEditor.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0331hw__memento_
+{
+    class TextEditor
+    {
+        private string State;
+
+        public TextEditor(string State)
+        {
+            this.State = State;
+        }
+
+        public IMemento Save()
+        {
+            return new TextMemento(this.State);
+        }
+        public void Rewrite(string NewState)
+        {
+            this.State = NewState;
+            Console.WriteLine("Text was changed to: "+ State);
+        }
+        public void Restore(IMemento memento)
+        {
+            if (!(memento is TextMemento))
+            {
+                throw new Exception("Unknown memento class " + memento.ToString());
+            }
+
+            this.State = memento.GetState();
+            Console.Write("Text was changed to:" + State);
+        }
+    }
+    public interface IMemento
+    {
+
+        string GetState();
+
+    }
+
+    class TextMemento : IMemento
+    {
+        private string State;
+
+        public TextMemento(string state)
+        {
+            this.State = state;
+        }
+
+        public string GetState()
+        {
+            return this.State;
+        }
+    }
+
+    class Caretaker
+    {
+        private List<IMemento> Mementos = new List<IMemento>();
+
+        private TextEditor Text = null;
+
+        public Caretaker(TextEditor originator)
+        {
+            this.Text = originator;
+        }
+
+        public void Backup()
+        {
+            this.Mementos.Add(this.Text.Save());
+        }
+
+        public void Undo()
+        {
+            if (this.Mementos.Count == 0)
+            {
+                return;
+            }
+
+            var Memento = this.Mementos.Last();
+            this.Mementos.Remove(Memento);
+
+            Console.WriteLine("Restoring state to: " + Memento.GetState());
+
+            try
+            {
+                this.Text.Restore(Memento);
+            }
+            catch (Exception)
+            {
+                this.Undo();
+            }
+        }
+    }
+
+}
